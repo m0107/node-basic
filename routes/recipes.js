e5e2302b6515f1c0b5bd0290898aee0106df56a6
mohit gupta665 <mohit.gupta665@gmail.com>
2021-07-28 01:09:22 +0530

    test

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,7 +3,10 @@ var router = require('express').Router();
 
 
 
-
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.status(200).send('All Good');
+  });
 
 module.exports = router;
 
